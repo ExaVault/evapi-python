a4f7fdb216659785fe355befb48b077664d67e78
David Ordal <dordal@exavault.com>
2020-11-03 14:43:17 -0800

    updated setup.py to v2.1.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ else:
 setup(
 
     name='exavault',
-    version='2.1.0',
+    version='2.1.2',
 
     description='Python SDK for interacting with ExaVault API',
     long_description=long_description,
